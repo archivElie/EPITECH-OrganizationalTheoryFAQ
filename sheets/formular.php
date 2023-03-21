3397a61f87b55bcd3fb4c72978e8fcebf1b48dac
Elie-Renneson <baptiste.renneson@gmail.com>
2023-03-21 11:26:17 +0100

    pushing the sheet

---
+++ sheets/formular.php
@@ -0,0 +1,34 @@
+<h1>Formulaire ORGTheory</h1>
+<form action="" method="post">
+  <label for="nom_prenom">Quel est votre nom et votre prénom ?</label><br>
+  <input type="text" id="nom_prenom" name="nom_prenom"><br>
+
+  <label for="fonction">Quelle fonction occupez-vous au sein de l’entreprise?</label><br>
+  <input type="text" id="fonction" name="fonction"><br>
+
+  <label for="duree_fonction">Depuis combien de temps occupez-vous cette fonction ?</label><br>
+  <input type="text" id="duree_fonction" name="duree_fonction"><br>
+
+  <label for="autre_fonction">Avez-vous toujours occupé cette fonction ? Si non, quelle fonction avez-vous occupé auparavant ?</label><br>
+  <textarea id="autre_fonction" name="autre_fonction"></textarea><br>
+
+  <label for="communication_interne">Comment communiquez-vous au sein de l’entreprise ?</label><br>
+  <textarea id="communication_interne" name="communication_interne"></textarea><br>
+
+  <label for="plateforme_communication">Quelle plateforme préférez-vous pour communiquer au sein de l’entreprise ? Si plusieurs, précisez lesquelles et pourquoi :</label><br>
+  <textarea id="plateforme_communication" name="plateforme_communication"></textarea><br>
+
+  <label for="communication_externe">Comment communiquez-vous avec les acteurs extérieurs à l’entreprise (client.es, associé.es?)</label><br>
+  <textarea id="communication_externe" name="communication_externe"></textarea><br>
+
+  <label for="effectif_equipe">De combien de personnes votre équipé est-elle composée ?</label><br>
+  <input type="text" id="effectif_equipe" name="effectif_equipe"><br>
+
+  <label for="effectif_entreprise">De combien de personnes votre entreprise est composée (question non obligatoire si petite entreprise) ?</label><br>
+  <input type="text" id="effectif_entreprise" name="effectif_entreprise"><br>
+
+  <label for="prise_decision">Comment les décisions importantes sont-elles prises au sein de votre entreprise (exemple : réunion, dossiers, présentations…) ?</label><br>
+  <textarea id="prise_decision" name="prise_decision"></textarea><br>
+
+  <input type="submit" value="Soumettre">
+</form>
